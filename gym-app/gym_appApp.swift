466512a2cdbd0463f258c354adea984b2800e90d
caaatherineb <cbaa2021@mymail.pomona.edu>
2024-06-29 02:11:44 -0700

    Initial Commit

---
+++ gym-app/gym_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  gym_appApp.swift
+//  gym-app
+//
+//  Created by Catherine Byen on 6/29/24.
+//
+
+import SwiftUI
+
+@main
+struct gym_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

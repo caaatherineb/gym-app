569f1dea2cfc9bb8062c88e080e3d3e2604025d2
caaatherineb <cbaa2021@mymail.pomona.edu>
2024-06-29 03:54:37 -0700

    home page first commit

--- gym-app/ContentView.swift
+++ gym-app/ContentView.swift
@@ -9,13 +9,81 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack {
+            Color(red: 0, green: 0.329, blue: 0.604).opacity(0.41).ignoresSafeArea()
+            
+            VStack{
+                Spacer()
+                ZStack{
+                    Rectangle()
+                        .foregroundColor(Color(red:1.0, green: 0.7843, blue: 0.5765))
+                        .cornerRadius(40)
+                        .frame(width: 250.0, height: 250.0)
+                    VStack{
+                        Text("Facility Hours")
+                            .font(.title)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.semibold/*@END_MENU_TOKEN@*/)
+                            .frame(alignment: .leading)
+                        HStack{
+                            Text("Sun")
+                                .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+                                .frame(alignment: .leading)
+                            Text("   9:00 AM - 5:00 PM")
+                        }
+                        HStack{
+                            Text("Mon")
+                                .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+                                .frame(alignment: .leading)
+                            Text("   9:00 AM - 5:00 PM")
+                        }
+                        HStack{
+                            Text("Tue")
+                                .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+                                .frame(alignment: .leading)
+                            Text("   9:00 AM - 5:00 PM")
+                        }
+                        HStack{
+                            Text("Wed")
+                                .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+                                .frame(alignment: .leading)
+                            Text("   9:00 AM - 5:00 PM")
+                        }
+                        HStack{
+                            Text("Thurs")
+                                .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+                                .frame(alignment: .leading)
+                            Text("   9:00 AM - 5:00 PM")
+                        }
+                        HStack{
+                            Text("Fri")
+                                .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+                                .frame(alignment: .leading)
+                            Text("   9:00 AM - 5:00 PM")
+                        }
+                        HStack{
+                            Text("Sat")
+                                .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+                                .frame(alignment: .leading)
+                            Text("   9:00 AM - 5:00 PM")
+                        }
+                        
+                    }
+                    
+                }
+                
+                Spacer()
+                Spacer()
+                Spacer()
+                Rectangle()
+                    .padding(.bottom, 10.0)
+                    .foregroundColor(Color(red: 0, green: 0.329, blue: 0.604))
+                    .cornerRadius(40)
+                    .ignoresSafeArea()
+                
+            }
         }
-        .padding()
+        
     }
 }
 
